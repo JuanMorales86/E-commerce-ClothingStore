068020bf66b5dfbd9c77e854e0c5df7898c21ff3
juanjmg <juanjosemorales1986@gmail.com>
2023-08-22 20:08:31 -0300

    RP

--- src/App.jsx
+++ src/App.jsx
@@ -17,9 +17,9 @@ import { Box, ThemeProvider } from '@mui/material'
 import 'react-toastify/dist/ReactToastify.css'
 
 //Mis componentes
-import CustomTheme from './components/custom-styles/themes'
+import CustomTheme from './components/Custom-Styles/themes'
 import HomePage from './components/home/Home'
-import NavBar from './components/navbar/navBar'
+import NavBar from './components/navbar/navbar'
 import Footer from './components/footer'
 import Nosotros from './components/nosotros'
 import Contacto from './components/contacto';
