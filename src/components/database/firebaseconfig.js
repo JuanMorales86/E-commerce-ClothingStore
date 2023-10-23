df336b7092ca96c098316230e4bbde4f8767b7ef
juanjmg <juanjosemorales1986@gmail.com>
2023-10-23 14:52:36 -0300

    bd

---
+++ src/components/database/firebaseconfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: "clothingstoredb-5a6f8.firebaseapp.com",
+    projectId: "clothingstoredb-5a6f8",
+    storageBucket: "clothingstoredb-5a6f8.appspot.com",
+    messagingSenderId: "185821558345",
+    appId: process.env.REACT_APP_FIREBASE_API_ID,
+  };
+  
+  export default firebaseConfig;
