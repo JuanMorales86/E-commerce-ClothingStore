1a96ed23173417b9e96af72fa54f440cc2346c68
juanjmg <juanjosemorales1986@gmail.com>
2023-11-22 18:30:19 -0300

    pruebas problemas mobile scroller

--- src/components/utilities/infinitescroller.jsx
+++ src/components/utilities/infinitescroller.jsx
@@ -79,25 +79,25 @@ function InfiniteScroller() {
 
       <div className="scroller"  data-speed="slow" >
         <div className="scroller__inner">
-          <img src="https://i.imgur.com/EetOdLJ.png" alt="" />
-          <img src="https://i.imgur.com/iOhT4Mo.png" alt="" />
-          <img src="https://i.imgur.com/TbyKuVP.png" alt="" />
-          <img src="https://i.imgur.com/54W7AWD.png" alt="" />
-          <img src="https://i.imgur.com/K8Z6cqF.png" alt="" />
-          <img src="https://i.imgur.com/hRBFJeZ.png" alt="" />
-          <img src="https://i.imgur.com/ELn3AIA.png" alt="" />
-          <img src="https://i.imgur.com/9LiuYxS.png" alt="" />
-          <img src="https://i.imgur.com/Nj3BuWf.png" alt="" />
-          <img src="https://i.imgur.com/MykEBZ9.png" alt="" />
-          <img src="https://i.imgur.com/3mwrK4e.png" alt="" />
-          <img src="https://i.imgur.com/uDsZjwD.png" alt="" />
-          <img src="https://i.imgur.com/hzaFJ76.png" alt="" />
-          <img src="https://i.imgur.com/pjBztBg.png" alt="" />
-          <img src="https://i.imgur.com/E2sRyiP.png" alt="" />
-          <img src="https://i.imgur.com/Qm3AJj4.png" alt="" />
-          <img src="https://i.imgur.com/Nee6MyI.png" alt="" />
-          <img src="https://i.imgur.com/xLXYU3P.png" alt="" />
-          <img src="https://i.imgur.com/3i0BLJZ.png" alt="" />
+          <img src="https://i.imgur.com/EetOdLJ.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/iOhT4Mo.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/TbyKuVP.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/54W7AWD.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/K8Z6cqF.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/hRBFJeZ.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/ELn3AIA.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/9LiuYxS.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/Nj3BuWf.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/MykEBZ9.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/3mwrK4e.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/uDsZjwD.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/hzaFJ76.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/pjBztBg.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/E2sRyiP.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/Qm3AJj4.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/Nee6MyI.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/xLXYU3P.png" alt="" loading='lazy'/>
+          <img src="https://i.imgur.com/3i0BLJZ.png" alt="" loading='lazy'/>
         </div>
       </div>
       </div>
